7eb13e1222e6c67c8eaf15b0c7ae3c10d440cce0
anatoli <anatoli@wsb-167-72a.icx.local>
2021-05-03 11:26:11 +0300

    add test to database

--- app/connect_db.py
+++ app/connect_db.py
@@ -21,6 +21,9 @@ def update_language_target_for_user(user_id, language):
 
 
 def add_new_user(user_id, language_target="en", language_from="ru"):
+    print(user_id)
+    print(language_target)
+    print(language_from)
     cursor.execute("INSERT INTO user_language (user_id, language_target, language_from) VALUES (%(user_id)s, %(language_target)s, %(language_from)s)",
                    {'user_id': user_id,
                     'language_target': language_target,
@@ -51,3 +54,9 @@ def get_language_from(user_id):
     cursor.execute("SELECT language_from FROM user_language WHERE user_id = %(user_id)s",
                    {'user_id': user_id})
     return cursor.fetchone()[0]
+
+
+def delete_user(user_id):
+    cursor.execute("DELETE FROM user_language WHERE user_id = %(user_id)s",
+                   {'user_id': user_id})
+    connection.commit()
